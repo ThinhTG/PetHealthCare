58e08ecabdf25e3e4fe6da9abad9db9df633c993
ThinhTG <thinhtgse173566@fpt.edu.vn>
2024-06-08 22:29:47 +0700

    done resetPass

---
+++ src/main/java/com/pethealthcare/demo/dto/request/ForgotPasswordRequest.java
@@ -0,0 +1,12 @@
+package com.pethealthcare.demo.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ForgotPasswordRequest {
+    private String email;
+}
