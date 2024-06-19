3cf9fa16f54101668b3e9ef7c3b36714938aaf0d
ThinhTG <thinhtgse173566@fpt.edu.vn>
2024-06-19 13:00:05 +0700

    Booking History Done

---
+++ src/main/java/com/pethealthcare/demo/service/BookingDetailService.java
@@ -0,0 +1,19 @@
+package com.pethealthcare.demo.service;
+
+import com.pethealthcare.demo.model.BookingDetail;
+import com.pethealthcare.demo.responsitory.BookingDetailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookingDetailService {
+    @Autowired
+    private BookingDetailRepository bookingDetailRepository;
+
+
+    public List<BookingDetail> getAllBookingDetail() {
+        return bookingDetailRepository.findAll();
+    }
+}
