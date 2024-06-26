b1a7d7db0a5af5da2b16e070c64f62a501424bba
annt1303 <anngtr1303@gmail.com>
2024-06-26 17:31:49 +0700

    add function update booking status, edit ServiceCreateRequest

--- src/main/java/com/pethealthcare/demo/responsitory/BookingRepository.java
+++ src/main/java/com/pethealthcare/demo/responsitory/BookingRepository.java
@@ -11,4 +11,6 @@ import java.util.List;
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
 
     List<Booking> getBookingByUser(User user);
+
+    Booking findBookingByBookingId(int bookingId);
 }
