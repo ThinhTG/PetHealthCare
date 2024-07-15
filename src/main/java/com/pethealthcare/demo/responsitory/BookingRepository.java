decb6a4325666c3b3c017a0bba6116d2f95d70c2
ThinhTG <thinhtgse173566@fpt.edu.vn>
2024-07-15 16:03:22 +0700

    REVENUE

--- src/main/java/com/pethealthcare/demo/responsitory/BookingRepository.java
+++ src/main/java/com/pethealthcare/demo/responsitory/BookingRepository.java
@@ -18,7 +18,7 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {
     List<Booking> getBookingByUser(User user);
     Booking findBookingByBookingId(int bookingId);
     List<Booking> findByDateBetween(LocalDate startDate, LocalDate endDate);
-    @Query(value = "SELECT new com.pethealthcare.demo.dto.request.RevenueResponse(MONTH(b.date), SUM(b.totalPrice) AS AMOUNT)\n" +
+    @Query(value = "SELECT new com.pethealthcare.demo.dto.request.RevenueResponse(MONTH(b.date), SUM(b.totalPrice))\n" +
             "FROM Booking b \n" +
             "WHERE YEAR(b.date) = :year \n" +
             "GROUP BY MONTH(b.date)  \n" +
